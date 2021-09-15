812879e62c1a8099f0321b696f758470fe7a8027
omolola.omolambe <omolola.omolambe@teamapt.com>
2021-09-15 16:38:28 +0100

    Startup Message Edited

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -42,7 +42,7 @@ const start = async () => {
 
 
     app.listen(3000, () => {
-        console.log("Listening on 3000!!!!!!!!!!!!!");
+        console.log("Listening on 3000!!!!!!!");
     });
 }
 
